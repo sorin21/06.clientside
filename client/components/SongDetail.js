d984cb26468fc20619a9ea346101c80027c3eca6
Sorin Dragomir <sorin.dragomir@Sorins-MacBook-Pro.local>
2018-09-24 15:57:32 +0300

    Lesson 62

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import fetchSong from '../queries/fetchSong';
+import LyricCreate from './LyricCreate';
 
-export default class SongDetail extends Component {
+ class SongDetail extends Component {
   render() {
+    const {song} = this.props.data;
+
+    // if is still loading
+    if(!song) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        <p>Song Detail</p>
+        <Link to="/">Back</Link>
+        <p>{song.title}</p>
+        <LyricCreate songId={this.props.params.id} />
       </div>
     )
   }
 }
+
+export default graphql(fetchSong, {
+  options: (props) => {
+    return {
+      variables: { id: props.params.id}
+    }
+  }
+})(SongDetail);
