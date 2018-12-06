76644ece22a1c779797ae15e50e597e0a51c3620
Sorin Dragomir <sorinel.dragomir@ownzones.com>
2018-12-06 16:01:42 +0200

    36

--- client/index.js
+++ client/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory, IndexRoute} from 'react-router';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import './style/style.css';
 import ApolloClient from 'apollo-client';
-import {ApolloProvider} from 'react-apollo';
+import { ApolloProvider } from 'react-apollo';
 import App from './components/App';
 import SongList from './components/SongList';
 import SongCreate from "./components/SongCreate";
 import SongDetail from "./components/SongDetail";
 
+// Is making request for data and storing data locally when the response came back
+// Assumes that the GraphQL Sever is available on /graphql route
 const client = new ApolloClient({
   // every time when we make a query
   // we ask for id for every record
@@ -19,6 +21,9 @@ const client = new ApolloClient({
 
 const Root = () => {
   return (
+    // Makes the GraphQL client available to any of your components
+    // It provides an ApolloClient instance to all of your GraphQL components
+    // client = we passing a referince to Apollo Store
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
         <Route path="/" component={App}>
--- server/server.js
+++ server/server.js
@@ -16,8 +16,8 @@ if (!MONGO_URI) {
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
